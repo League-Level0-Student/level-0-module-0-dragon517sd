802c0a3217d23413ec600233f98fbce058943288
user <user@Kitchen>
2021-03-20 18:01:35 -0700

    Pentagon Crazy

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -9,37 +9,40 @@ import java.awt.Color;
 
 public class PentagonCrazy {
 	
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		//  Create a new Robot
-
+		Robot robot=new Robot();
 		//  Put the robot's pen down
-
+		robot.penDown ();
 		//  SPEED. Make the robot go at maximum speed (100)
-
+		robot.setSpeed(100);
 		//  COLOR. Set the pen to a color that you like for the shape
-
+		robot.setPenColor(Color.ORANGE);
 		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape will have.
 		//  				(Hint: its called PentagonCrazy)
-
+		int sides=5;
 		//  TURN ANGLE. Make another int variable for the angle the robot must turn. 
 		//  			Hint: Divide 360 by the number of sides the shape has to get the angle.
-		
+		int angle=360/sides;
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-
+		int count=0;
 		//  LOOP. Start a while loop to repeat the DRAW, TURN, and INCREASE COUNT code 200 times. 
-		
+		while (count<200) {
 				//  DRAW.  Make the robot move the amount in your count variable
-		
+			robot.move(count);
 				//  TURN.  Turn the robot the amount in your angle variable + 1
-
+			robot.turn (angle+1);
 				//  INCREASE COUNT. Increase the count by 1
-		
+		count++;
 		//  End the while loop here
-
+		}
 		//  Now hide the robot so you can see the pattern it has drawn.
-		
+		robot.hide();
 		//  Run the program. 
 		//  Check the pattern against the picture in the recipe. If it matches, you are done!
 	
