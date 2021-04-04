d8ced9b4e3b9b276064410ec0a4681d599418423
user <user@Kitchen>
2021-04-03 17:40:29 -0700

    Quiz game and Awesome or Not

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,39 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score=0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+				String input=JOptionPane.showInputDialog("Which planet has the highest temperature?");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if (input.equals("Venus")) {
+					JOptionPane.showMessageDialog(null, "Correct!");
+					score++;
+				}
+				else score--;
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
-		
+		input=JOptionPane.showInputDialog("Which is the farthest dwarf planet in our solar system?");
+		if (input.equals("Sedna")) {
+			JOptionPane.showMessageDialog(null, "That's right");
+			score+=1;
+		}
+		else score-=1;
+		input=JOptionPane .showInputDialog("Which planets/moons other than Earth may or may have possibly hosted life?");
+			if (input.equals("Venus, Mars, and Europa")) {
+				JOptionPane.showMessageDialog(null, "Cool!");
+				score=score+1;
+			}
+			else score=score-1;
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "Score is "+score);
 	}
 }
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,25 +2,34 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
 
+	public static void main(String[] args) {
+		
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-	
+		int numb=0;
+		System.out.println(numb);
+		System.out.println(numb++);
+		System.out.println(++numb);
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
+		numb=ran.nextInt(4);
 		// 3. Print your variable to the console
-	
+		System.out.println(numb);
 		// 4. Get the user to enter something that they think is awesome
-	
+		String input=JOptionPane.showInputDialog("Enter something that's awesome!!");
 		// 5. If your variable is  0
-	
+		if (numb==0) {
 			// -- tell the user whatever they entered is awesome!
-	
+			JOptionPane.showMessageDialog(null, input+" is awesome!");
+		}
+		
 		// 6. If your variable is  1
 	
 			// -- tell the user whatever they entered is ok.
@@ -32,5 +41,5 @@ public class AwesomeOrNot {
 		// 8. If your variable is  3
 	
 			// -- invent your own message to give to the user (be nice).
-
+	}
 }
